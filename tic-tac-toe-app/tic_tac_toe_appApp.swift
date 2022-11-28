844a2bc192dc29fcba5bd609c778da6a97421f8c
gabrielsantos <gjs8@aluno.ifal.edu.br>
2022-11-28 11:55:49 -0300

    Add initial project

---
+++ tic-tac-toe-app/tic_tac_toe_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  tic_tac_toe_appApp.swift
+//  tic-tac-toe-app
+//
+//  Created by Gabriel Santos on 28/11/22.
+//
+
+import SwiftUI
+
+@main
+struct tic_tac_toe_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
